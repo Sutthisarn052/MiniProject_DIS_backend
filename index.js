112ffab4d8123e1606d221db08cd4d3b31182811
Sutthisarn052 <6035512007@psu.ac.th>
2022-04-22 23:00:45 +0700

    Day2_1

--- index.js
+++ index.js
@@ -18,9 +18,54 @@ router.use(cors({ origin: "http://localhost:3000", credentials: true }));
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-router.get("/test", async (req, res) => {
-    console.log("test1");
-   
-  });
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    console.log("Login: ", req.body, user, err, info);
+    if (err) return next(err);
+    if (user) {
+      if (req.body.remember == true) {
+        time_exp = "7d";
+      } else time_exp = "1d";
+      const token = jwt.sign(user, db.SECRET, {
+        expiresIn: time_exp,
+      });
+      var decoded = jwt.decode(token);
+      let time = new Date(decoded.exp * 1000);
+      console.log(new Date(decoded.exp * 1000));
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60,
+          sameSite: "strict",
+          path: "/",
+        })
+      );
+      res.statusCode = 200;
+      return res.json({ user, token });
+    } else return res.status(422).json(info);
+  })(req, res, next);
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const SALT_ROUND = 10;
+    const { username, email, password } = req.body;
+    if (!username || !email || !password)
+      return res.json({ message: "Cannot register with empty string" });
+    if (db.checkExistingUser(username) !== db.NOT_FOUND)
+      return res.json({ message: "Duplicated user" });
+
+    let id = users.users.length
+      ? users.users[users.users.length - 1].id + 1
+      : 1;
+    hash = await bcrypt.hash(password, SALT_ROUND);
+    users.users.push({ id, username, password: hash, email });
+    res.status(200).json({ message: "Register success" });
+  } catch {
+    res.status(422).json({ message: "Cannot register" });
+  }
+});
 
   app.listen(port, () => console.log(`Server is running on port ${port}`));
